0a3e2c84950acd379ebc5cd505aa417d7a20b731
Donghua.Lau <me@liudonghua.net>
2017-08-29 21:24:50 +0800

    update code

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-function win_init()
-{
-	var win = nw.Window.get();
-	win.showDevTools();
-	console.log('Window is minimized');
+var Imap = require('imap'),
+    inspect = require('util').inspect;
+
+var imap = new Imap({
+  user: '519916178@qq.com',
+  password: 'HUA9537PX',
+  host: 'imap.qq.com',
+  port: 993,
+  tls: true
+});
+
+/*
+function openInbox(cb) {
+  imap.openBox('INBOX', true, cb);
 }
+
+imap.once('ready', function() {
+	console.log('imap ready');
+});
+
+imap.once('error', function(err) {
+  console.log(err);
+});
+
+imap.once('end', function() {
+  console.log('Connection ended');
+});
+*/
+
+imap.connect();
+console.log('imap connect');
